fcced083949b1f70f8e3b856b310e877a8c5f983
Niccolò Marastoni <niccolo.marastoni@gmail.com>
2025-11-18 15:50:51 +0100

    first version of the recommender using the LLM

--- qr_api/resources/endpoints.py
+++ qr_api/resources/endpoints.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
 from qr_api.resources.data_models import QueryRequest, QueryResponse, UserProfile
-import json
+import json, requests
 from pathlib import Path
+import os
 
 router = APIRouter()
 
-def generate_next_queries(current_query: str, context: dict) -> str:
+def local_generate_next_queries(current_query: str, context: dict) -> str:
     """
     Inputs:
     current_query  <- a string that contains the current NL query submitted by the user
@@ -23,6 +24,50 @@ def generate_next_queries(current_query: str, context: dict) -> str:
     user_profile = load_user_profile_from_json(user_profile_file_path)
     
     return [current_query, 'next query']
+
+def generate_next_queries(current_query: str, context: dict) -> list[str]:
+    payload = {
+            "model": "llama-4-maverick",
+            "messages": [
+                {
+                    "role":"system",
+                    "content": '''You are an AI assistant whose primary goal is to suggest the next search queries, in order to help a user search and find information better on a search engine. 
+                    Two different queries and entities are separated by the token '|'. For example,'Microsoft' and 'Google' would appear as 'Microsoft' | 'Google'.
+                    Only suggest a list of the next 3 (maximum) queries, in the format: ['query1', 'query2', 'query3']. Make them as short as possible. Do not repeat or summarize the current query.
+                    '''
+    
+                },
+                {
+                    "role":"user",
+                    "content":f'''Current query: {current_query}'''
+                }
+            ]
+        }
+    
+    TOKEN = os.getenv('LLM_TOKEN', '')
+    LLM_API_URL = os.getenv('LLM_API_URL', '')
+    HEADERS = {
+    "User-Agent": "Python API Sample",
+    "Authorization": "Bearer " + TOKEN,
+    "Content-Type": "application/json"
+    }
+    json_data = json.dumps(payload).encode('utf8')
+    #print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
+    
+    #send POST request to the FastAPI endpoint
+    try:
+        response = requests.post(url=LLM_API_URL, headers=HEADERS, data=json_data, verify=False)
+        response.raise_for_status()
+        res = response.json()['choices'][0]['message']['content']
+        
+        res = res.replace('[', '').replace(']', '').replace("'", '').split(',')
+        print('RESPONSE:', res)
+        print('TYPE:', type(res))
+        return res
+    
+    except requests.exceptions.RequestException as e:
+        print("Error while calling API:", e)
+        return None
 
 
 def load_user_profile_from_json(file_path: str) -> UserProfile:
@@ -43,4 +88,5 @@ def load_user_profile_from_json(file_path: str) -> UserProfile:
 @router.post("/recommend-next-query", response_model=QueryResponse)
 async def recommend_next_query(request: QueryRequest):
     next_queries = generate_next_queries(request.current_query, request.context)
+    print('TYPE next queries', type(next_queries))
     return QueryResponse(next_queries=next_queries)
